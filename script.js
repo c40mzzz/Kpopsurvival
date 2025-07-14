edeab6a1645311bb6221af5b581767bc155c62d7
c40mzzz <heesungrr@gmail.com>
2025-07-14 18:30:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+javascript
+document.getElementById('start-btn').addEventListener('click', function() {
+    alert('Iniciando simulación...');
+    // Aquí puedes redirigir a otra página o iniciar la simulación
+});
